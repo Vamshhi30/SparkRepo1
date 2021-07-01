42da213efbdcafed151f29d636ea1211678bca4a
Vamshhi <Vamshhi@DESKTOP-FU0BI74>
2021-07-01 13:25:46 +0530

    Spark DataFrame API Task 3

---
+++ SparkDataFrameAPIs/src/main/scala/com/vamshhi/spark/SparkDataFrameAPI_Task3.scala
@@ -0,0 +1,17 @@
+package com.vamshhi.spark
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object SparkDataFrameAPI_Task3 {
+
+	def main(args:Array[String]):Unit ={
+			val spark = SparkSession.builder().appName("Spark Dataframe APIs Task3").master("local[*]").getOrCreate()
+					val txns_DF = spark.read.format("csv").option("header","true").option("inferschema","true").load("file:///C:/Data/txns")
+					//txns_DF.show()
+					val txns_filter = txns_DF.filter(!col("category").isin("Gymnastics","Team Sports"))
+					//txns_filter.show()
+					val txns_res = txns_filter
+					               .withColumn("cat1",expr("case when category = 'Exercise & Fitness' or category = 'Outdoor Recreation' then 'Outdoor' else 'Indoor' end"))
+					txns_res.show()   
+	}
+}
