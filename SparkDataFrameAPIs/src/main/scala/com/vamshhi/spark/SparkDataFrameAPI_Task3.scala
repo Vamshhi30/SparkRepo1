1e62d6ee195edb0f2a7c502fe26a59754215500e
Vamshhi <Vamshhi@DESKTOP-FU0BI74>
2021-07-01 13:35:45 +0530

    Updated args

--- SparkDataFrameAPIs/src/main/scala/com/vamshhi/spark/SparkDataFrameAPI_Task3.scala
+++ SparkDataFrameAPIs/src/main/scala/com/vamshhi/spark/SparkDataFrameAPI_Task3.scala
@@ -6,12 +6,12 @@ object SparkDataFrameAPI_Task3 {
 
 	def main(args:Array[String]):Unit ={
 			val spark = SparkSession.builder().appName("Spark Dataframe APIs Task3").master("local[*]").getOrCreate()
-					val txns_DF = spark.read.format("csv").option("header","true").option("inferschema","true").load("file:///C:/Data/txns")
+					val txns_DF = spark.read.format("csv").option("header","true").option("inferschema","true").load(args(0))
 					//txns_DF.show()
 					val txns_filter = txns_DF.filter(!col("category").isin("Gymnastics","Team Sports"))
 					//txns_filter.show()
-					val txns_res = txns_filter
-					               .withColumn("cat1",expr("case when category = 'Exercise & Fitness' or category = 'Outdoor Recreation' then 'Outdoor' else 'Indoor' end"))
-					txns_res.show()   
+					val txns_res = txns_filter.withColumn("cat1",expr("case when category = 'Exercise & Fitness' or category = 'Outdoor Recreation' then 'Outdoor' else 'Indoor' end"))
+					//txns_res.show()
+					txns_res.coalesce(1).write.format("com.databricks.spark.avro").mode("overwrite").save(args(1))             
 	}
 }
